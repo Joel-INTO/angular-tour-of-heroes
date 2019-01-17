8a0fcacc6a0ea100320ddee97f4da2c103e1fbec
joelAirMac <joel.balmer@gmail.com>
2019-01-17 21:23:35 +0000

    4. up to 'show messages'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: "app-heroes",
@@ -8,14 +8,21 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() {}
+  getHeroes(): void {
+    // It's basically saying "get this method (getHeroes), asynchronously, and assign the result (heroes) to our heroes property (this.heroes) "
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
 
-  ngOnInit() {}
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
