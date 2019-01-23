e04398f0b97ca612cd085a66096e83fb901c24ae
Joel <joel.balmer@intoglobal.com>
2019-01-23 17:36:51 +0000

    End of 5 tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { Hero } from "../hero";
-import { HeroService } from "../hero.service";
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css", "./../messages/messages.component.css"]
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css', './../messages/messages.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
+  /*
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
-  getHeroes(): void {
-    // It's basically saying "get this method (getHeroes), asynchronously, and assign the result (heroes) to our heroes property (this.heroes) "
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-  }
+  */
 
   constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    // It's basically saying "get this method (getHeroes), asynchronously,
+    // and assign the result (heroes) to our heroes property (this.heroes)
+    // and it returns an observable, which looks for promise changes, so it
+    // can be updated
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
 
   ngOnInit() {
     this.getHeroes();
