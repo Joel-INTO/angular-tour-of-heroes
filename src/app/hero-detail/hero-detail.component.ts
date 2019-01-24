a7fdd8838bc5b24bb7c194523d16a5c5dd087972
Joel <joel.balmer@intoglobal.com>
2019-01-24 12:10:11 +0000

    6. Http, have done up until here: https://angular.io/tutorial/toh-pt6#http-methods-return-one-value

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -21,8 +21,8 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+    // this.heroService.getHero(id)
+    //  .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
